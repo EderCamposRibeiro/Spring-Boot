f7b449c21048a071fab604ec5033099c1031e446
Eder Campos Ribeiro <edercribeiro@gmail.com>
2020-10-12 11:34:03 -0300

    Charging our person's list

--- src/main/java/project/springboot/controller/PersonController.java
+++ src/main/java/project/springboot/controller/PersonController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import project.springboot.model.Person;
 import project.springboot.repository.PersonRepository;
@@ -23,6 +24,14 @@ public class PersonController {
 	public String save(Person person) {
 		personRepository.save(person);
 		return "register/personregister";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/personslist")
+	public ModelAndView persons() {
+		ModelAndView andView = new ModelAndView("register/personregister");
+		Iterable<Person> personsIt = personRepository.findAll();
+		andView.addObject("persons", personsIt);
+		return andView;
 	}
 
 }
