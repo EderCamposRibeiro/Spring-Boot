c447ceaeb6005831d8e33ceab56bcc77dc746e5b
Eder Campos Ribeiro <edercribeiro@gmail.com>
2020-10-13 20:04:43 -0300

    Editing register with Thymeleaf and Spring

--- src/main/java/project/springboot/controller/PersonController.java
+++ src/main/java/project/springboot/controller/PersonController.java
@@ -1,7 +1,11 @@
 package project.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,18 +20,20 @@ public class PersonController {
 	private PersonRepository personRepository;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/personregister")
-	public String begin() {
-		return "register/personregister";
+	public ModelAndView begin() {
+		ModelAndView andView = new ModelAndView("register/personregister");
+		andView.addObject("personobj", new Person());
+		return andView;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/saveperson")
+	@RequestMapping(method = RequestMethod.POST, value = "**/saveperson")
 	public ModelAndView save(Person person) {
 		personRepository.save(person);
 		
 		ModelAndView andView = new ModelAndView("register/personregister");
 		Iterable<Person> personsIt = personRepository.findAll();
 		andView.addObject("persons", personsIt);
-		
+		andView.addObject("personobj", new Person());
 		return andView;
 	}
 	
@@ -36,7 +42,19 @@ public class PersonController {
 		ModelAndView andView = new ModelAndView("register/personregister");
 		Iterable<Person> personsIt = personRepository.findAll();
 		andView.addObject("persons", personsIt);
+		andView.addObject("personobj", new Person());
 		return andView;
 	}
+	
+	@GetMapping("/editperson/{idperson}")
+	public ModelAndView edit(@PathVariable("idperson") Long idperson) {
+
+		Optional<Person> person = personRepository.findById(idperson);
+		
+		ModelAndView andView = new ModelAndView("register/personregister");
+		andView.addObject("personobj", person.get());
+		return andView;
+	}
+	
 
 }
