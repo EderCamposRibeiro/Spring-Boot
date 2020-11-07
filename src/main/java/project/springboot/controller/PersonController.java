70fa06d31c0139f500067cc32895cafccd9a8094
Eder Campos Ribeiro <edercribeiro@gmail.com>
2020-11-07 10:14:32 -0300

    Solving Edit Problem

--- src/main/java/project/springboot/controller/PersonController.java
+++ src/main/java/project/springboot/controller/PersonController.java
@@ -122,7 +122,7 @@ public class PersonController {
 		andView.addObject("persons", personRepository.findAll(PageRequest.of(0, 5, Sort.by("name"))));
 		andView.addObject("professions", professionRepository.findAll());		
 		return andView;
-		//Somente para aparecer alguma mudança
+		//Somente para mudar
 	}
 	
 	@GetMapping("/deleteperson/{idperson}")
